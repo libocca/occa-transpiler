c518a590fdfdbdd2593a8fe0a4f530e3afdec6a7
Kristian Chaba <kchab@softserveinc.com>
2024-01-31 18:38:22 +0100

    Fix Attribute sequence.

--- tests/functional/data/normalize/dim/attr_use_ref.cpp
+++ tests/functional/data/normalize/dim/attr_use_ref.cpp
@@ -8,7 +8,7 @@ struct sMat24 {
 [[okl::kernel("(void)")]] void
 test([[okl::dimOrder("(1,0)")]] iPtr45 a, sMat24 *b,
      [[okl::dimOrder("(2,1,0)")]] iMat455 &ab,
-     [[okl::dimOrder("(0,1)")]] [[okl::dim("(4,5)")]] float *ac) {
+     [[okl::dim("(4,5)")]] [[okl::dimOrder("(0,1)")]] float *ac) {
   [[okl::outer("(void)")]] for (int i = 0; i < 4; ++i) {
     [[okl::shared("(void)")]] [[okl::dim("(4,5)")]] [[okl::dimOrder("(0,1)")]] int cc[5 * 4];
     [[okl::inner("(void)")]] for (int j = 0; j < 5; ++j) {
