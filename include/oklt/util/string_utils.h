2becbb368e379eb876bd35005bfd6b8b14d188a1
Kristian Chaba <kchab@softserveinc.com>
2024-02-15 16:59:55 +0100

    Add useful string util functions.

--- include/oklt/util/string_utils.h
+++ include/oklt/util/string_utils.h
@@ -1,9 +1,52 @@
 #pragma once
 
+#include <vector>
 #include <string>
+#include <optional>
+#include <charconv>
 
 namespace oklt::util {
 std::string toLower(const std::string& str);
 std::string toCamelCase(std::string str);
 std::string pointerToStr(const void* ptr);
+
+[[nodiscard]] std::string_view rtrim(std::string_view &str);
+[[nodiscard]] std::string_view ltrim(std::string_view &str);
+[[nodiscard]] std::string_view trim(std::string_view &str);
+[[nodiscard]] std::string_view unParen(std::string_view& str);
+[[nodiscard]] std::string_view slice(const std::string_view &str, size_t start, size_t end);
+[[nodiscard]] std::vector<std::string_view> split(const std::string_view& str, const std::string_view& sep, int maxN = -1,
+                                             bool keepEmpty = true);
+
+template<typename T, bool>
+std::optional<T> parseStrTo(std::string_view& str);
+
+template<typename T, std::enable_if_t<std::is_integral_v<T>, bool> = true>
+inline std::optional<T> parseStrTo(std::string_view& str) {
+    T ret;
+    if (std::from_chars(str.data(), str.data() + str.size(), ret).ec == std::errc{})
+        return ret;
+    return {};
+}
+
+template<typename T, std::enable_if_t<std::is_floating_point_v<T>, bool> = true>
+inline std::optional<T> parseStrTo(std::string_view& str) {
+    T ret;
+    if (std::from_chars(str.data(), str.data() + str.size(), ret).ec == std::errc{})
+        return ret;
+    return {};
+}
+
+template<typename T>
+inline std::optional<T> parseStrTo(const char *str) {
+    std::string_view s(str);
+    return parseStrTo<T>(s);
+}
+
+template<typename T>
+inline std::optional<T> parseStrTo(const std::string &str) {
+    std::string_view s(str);
+    return parseStrTo<T>(s);
+}
+
 }  // namespace oklt::util
--- lib/util/string_utils.cpp
+++ lib/util/string_utils.cpp
@@ -1,8 +1,14 @@
 #include <oklt/util/string_utils.h>
-#include <algorithm>
+
+#include <llvm/ADT/StringRef.h>
+#include <llvm/ADT/SmallVector.h>
+
+#include <charconv>
 #include <sstream>
 
 namespace oklt::util {
+using namespace llvm;
+
 std::string toLower(const std::string& str) {
     std::string result;
     result.reserve(str.size());
@@ -33,6 +39,47 @@ std::string pointerToStr(const void* ptr) {
     ss << ptr;
     std::string strPointer = ss.str();
     return strPointer;
+}
+
+std::string_view rtrim(std::string_view &str) {
+    return StringRef(str).rtrim();
+}
+
+std::string_view ltrim(std::string_view &str) {
+    return StringRef(str).ltrim();
+}
+
+std::string_view trim(std::string_view &str) {
+    return StringRef(str).trim();
+}
+
+std::string_view unParen(std::string_view& str) {
+    if (!str.empty() && str.front() == '(' && str.back() == ')') {
+        return str.substr(1, str.size() - 1);
+    }
+    return str;
+}
+
+std::vector<std::string_view> split(const std::string_view& str, const std::string_view& sep, int maxN, bool keepEmpty) {
+    std::vector<std::string_view> ret;
+
+    StringRef s(str);
+    while (maxN-- != 0) {
+        auto Idx = s.find(sep);
+        if (Idx == StringRef::npos) {
+            break;
+        }
+        if (keepEmpty || Idx > 0) {
+            ret.push_back(s.slice(0, Idx));
+        }
+        s = s.slice(Idx + 1, StringRef::npos);
+    }
+
+    if (keepEmpty || !s.empty()) {
+        ret.push_back(s);
+    }
+
+    return ret;
 }
 
 }  // namespace oklt::util
