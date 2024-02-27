dab2dc2cf962119eb7936463c50a33cf497256c1
Pavlo Hilei <phile@softserveinc.com>
2024-02-27 16:45:16 +0100

    Add @restrict to dpcpp

--- lib/attributes/backend/dpcpp/restrict.cpp
+++ lib/attributes/backend/dpcpp/restrict.cpp
@@ -1,11 +1,37 @@
 #include "attributes/attribute_names.h"
+#include "core/ast_processors/okl_sema_processor/okl_sema_ctx.h"
 #include "core/attribute_manager/attribute_manager.h"
+#include "core/transpiler_session/session_stage.h"
+#include "core/utils/attributes.h"
+#include "core/utils/range_to_string.h"
 
 namespace {
 using namespace oklt;
+using namespace clang;
+const std::string RESTRICT_MODIFIER = "__restrict__";
+
 HandleResult handleRestrictAttribute(const clang::Attr* a,
-                                     const clang::Decl* d,
+                                     const clang::ParmVarDecl* parmDecl,
                                      SessionStage& s) {
+    auto& rewriter = s.getRewriter();
+
+    SourceLocation identifierLoc = parmDecl->getLocation();
+    removeAttribute(a, s);
+    std::string restrictText = " " + RESTRICT_MODIFIER + " ";
+    rewriter.InsertText(identifierLoc, restrictText, false, false);
+
+    // INFO: might be better to use rewriter.getRewrittenText() method
+
+    auto& ctx = parmDecl->getASTContext();
+    SourceRange r1(parmDecl->getSourceRange().getBegin(), identifierLoc);
+    auto part1 = getSourceText(r1, ctx);
+    auto ident = parmDecl->getQualifiedNameAsString();
+    std::string modifiedArgument = part1 + restrictText + ident;
+
+    if (s.getAstProccesorType() == AstProcessorType::OKL_WITH_SEMA) {
+        s.tryEmplaceUserCtx<OklSemaCtx>().setKernelArgRawString(parmDecl, modifiedArgument);
+    }
+
 #ifdef TRANSPILER_DEBUG_LOG
     llvm::outs() << "[DEBUG] DPCPP: Handle @restrict.\n";
 #endif
