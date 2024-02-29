86063034c5978605e0debdc1e7d2ba44f986656d
Pavlo Hilei <phile@softserveinc.com>
2024-02-29 13:24:12 +0100

    Remove commented replaceAttributedLoop

--- lib/attributes/utils/code_gen.cpp
+++ lib/attributes/utils/code_gen.cpp
@@ -43,30 +43,6 @@ std::string buildCloseScopes(int& openedScopeCounter) {
     return res;
 }
 
-// void replaceAttributedLoop(const clang::Attr& a,
-//                            const clang::ForStmt& f,
-//                            const std::string& prefixCode,
-//                            const std::string& suffixCode,
-//                            SessionStage& s) {
-//     auto& rewriter = s.getRewriter();
-
-//     // Remove attribute + for loop:
-//     //      @attribute(...) for (int i = start; i < end; i += inc)
-//     //  or: for (int i = start; i < end; i += inc; @attribute(...))
-//     clang::SourceRange range;
-//     range.setBegin(a.getRange().getBegin().getLocWithOffset(-2));  // TODO: remove magic number
-//     range.setEnd(f.getRParenLoc());
-//     rewriter.RemoveText(range);
-
-//     // Insert preffix
-//     rewriter.InsertText(f.getRParenLoc(), prefixCode);
-
-//     // Insert suffix
-//     rewriter.InsertText(f.getEndLoc(),
-//                         suffixCode);  // TODO: seems to not work correclty for for loop without
-//                         {}
-// }
-
 HandleResult replaceAttributedLoop(const clang::Attr& a,
                                    const clang::ForStmt& f,
                                    const std::string& prefixCode,
