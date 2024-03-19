34445f3634bf03228700d222d72642884a6b2cf7
kchabSS <156786434+kchabSS@users.noreply.github.com>
2024-03-19 21:50:50 +0100

    Fix/macro in kernel params (#106)
    * Fix CUDA, HIP, and DPCPP kernel parameter manipulations.
    * Add regression tests.

---
+++ tests/functional/data/transpiler/backends/cuda/macro/macro_in_param_ref.cpp
@@ -0,0 +1,10 @@
+#include <cuda_runtime.h>
+extern "C" __global__ void _occa_mykern_0(int aaa, int bbb) {
+    {
+        int i = (0) + blockIdx.x;
+        {
+            int j = (0) + threadIdx.x;
+            // BODY
+        }
+    }
+}
