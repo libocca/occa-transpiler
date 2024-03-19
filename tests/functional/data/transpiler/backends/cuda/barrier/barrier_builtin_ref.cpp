d210b20fa6998dca971072c7fb1fdcbfa5c4d47c
kchabSS <156786434+kchabSS@users.noreply.github.com>
2024-03-19 13:30:29 +0100

    Update barrier_builtin_ref.cpp

--- tests/functional/data/transpiler/backends/cuda/barrier/barrier_builtin_ref.cpp
+++ tests/functional/data/transpiler/backends/cuda/barrier/barrier_builtin_ref.cpp
@@ -7,9 +7,9 @@ __device__ static float add(const float *a, int i, const float *b, int j) {
 // TODO: fix preprocessor handling and try with define
 extern "C" __global__ void _occa_addVectors_0(const int N, const float *a,
                                               const float *b, float *ab) {
-    {
-        int i = (0) + ((4) * blockIdx.x);
-        __shared__ float s_b[4];
+  {
+    int i = (0) + ((4) * blockIdx.x);
+    __shared__ float s_b[4];
     const float *g_a = a;
     {
       int j = (0) + threadIdx.x;
