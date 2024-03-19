70c5b5c10dbb207dccdaec5831e8b6bf00822e13
kchabSS <156786434+kchabSS@users.noreply.github.com>
2024-03-19 13:31:14 +0100

    Update exclusive_builtin_ref.cpp

--- tests/functional/data/transpiler/backends/cuda/exclusive/exclusive_builtin_ref.cpp
+++ tests/functional/data/transpiler/backends/cuda/exclusive/exclusive_builtin_ref.cpp
@@ -10,15 +10,15 @@ __constant__ int BLOCK_SIZE = 4;
 
 extern "C" __global__ void _occa_addVectors_0(const int N, const float *a,
                                               const float *b, float *ab) {
-    {
-        int i = (0) + ((BLOCK_SIZE)*blockIdx.x);
-        __shared__ float s_b[BLOCK_SIZE];
+  {
+    int i = (0) + ((BLOCK_SIZE)*blockIdx.x);
+    __shared__ float s_b[BLOCK_SIZE];
     const float *g_a = a;
     {
       int j = (0) + threadIdx.x;
-        s_b[j] = b[i + j];
-        __syncthreads();
-        ab[i + j] = add(g_a, i + j, s_b, j);
+      s_b[j] = b[i + j];
+      __syncthreads();
+      ab[i + j] = add(g_a, i + j, s_b, j);
     }
   }
 }
