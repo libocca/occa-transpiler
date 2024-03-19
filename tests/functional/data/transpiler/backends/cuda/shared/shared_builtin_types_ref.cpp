b0dbb55267448b171c8a37507a5723a6cdf4b838
kchabSS <156786434+kchabSS@users.noreply.github.com>
2024-03-19 13:32:25 +0100

    Update shared_builtin_types_ref.cpp

--- tests/functional/data/transpiler/backends/cuda/shared/shared_builtin_types_ref.cpp
+++ tests/functional/data/transpiler/backends/cuda/shared/shared_builtin_types_ref.cpp
@@ -1,9 +1,9 @@
 #include <cuda_runtime.h>
 
 extern "C" __global__ void _occa_function1_0(const int *data) {
-    {
-        int i = (0) + blockIdx.x;
-        __shared__ int arr1[32];
+  {
+    int i = (0) + blockIdx.x;
+    __shared__ int arr1[32];
     __shared__ float arr2[8][32];
     __shared__ double arr3[4 + 4];
     {
