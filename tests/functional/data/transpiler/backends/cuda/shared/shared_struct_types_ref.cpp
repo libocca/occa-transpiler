2216b608c18b38ab9c39426f5a6b7ac20c11235a
kchabSS <156786434+kchabSS@users.noreply.github.com>
2024-03-19 13:33:18 +0100

    Update shared_struct_types_ref.cpp

--- tests/functional/data/transpiler/backends/cuda/shared/shared_struct_types_ref.cpp
+++ tests/functional/data/transpiler/backends/cuda/shared/shared_struct_types_ref.cpp
@@ -6,9 +6,9 @@ struct ComplexValueFloat {
 };
 
 extern "C" __global__ void _occa_function1_0(const int *data) {
-    {
-        int i = (0) + blockIdx.x;
-        __shared__ ComplexValueFloat arr2[8][32];
+  {
+    int i = (0) + blockIdx.x;
+    __shared__ ComplexValueFloat arr2[8][32];
     __shared__ ComplexValueFloat arr1[32];
     {
       int j = (0) + threadIdx.x;
