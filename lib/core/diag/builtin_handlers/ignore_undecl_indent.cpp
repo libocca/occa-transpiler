2bfe24e9c097de353e5a64f59939435c031bbf00
Iurii Kobein <ikobein@softserveinc.com>
2024-03-21 13:03:20 +0000

    allows function call to be undeclare var

--- lib/core/diag/builtin_handlers/ignore_undecl_indent.cpp
+++ lib/core/diag/builtin_handlers/ignore_undecl_indent.cpp
@@ -8,20 +8,59 @@ namespace {
 using namespace oklt;
 using namespace clang;
 
+bool isLooksLikeFunctionCall(const SourceLocation& loc,
+                             const SourceManager& sm,
+                             const LangOptions& lo) {
+    std::list<Token> tokens;
+    SourceLocation currLoc = loc;
+    while (1) {
+        auto maybeToken = Lexer::findNextToken(currLoc, sm, lo);
+        if (!maybeToken) {
+            return false;
+        }
+
+        if (maybeToken->is(tok::semi)) {
+            break;
+        }
+
+        if (maybeToken->isOneOf(tok::l_paren, tok::r_paren)) {
+            tokens.push_back(maybeToken.value());
+        }
+
+        currLoc = maybeToken->getLocation();
+    }
+
+    // at least two token for function call 'func()'
+    if (tokens.size() < 2) {
+        return false;
+    }
+
+    return tokens.front().is(tok::l_paren) && tokens.back().is(tok::r_paren);
+}
+
 class IgnoreUndeclHandler : public DiagHandler {
    public:
     IgnoreUndeclHandler()
         : DiagHandler(diag::err_undeclared_var_use){};
 
     bool HandleDiagnostic(SessionStage& session, DiagLevel level, const Diagnostic& info) override {
+        const auto& sm = info.getSourceManager();
+
+        // XXX In general OKL allows malformed source code to be parsed successfully
+        // For example undeclared variable/function calls are suppressed and let target back-end
+        // compiler figure out
+        // this design is questionable but anyway so far we obey legacy
+        if (!isLooksLikeFunctionCall(info.getLocation(), sm, session.getCompiler().getLangOpts())) {
+            return false;
+        }
         // TODO unify with error reporting to get correct location and source line
         llvm::SmallString<64> buf;
         info.FormatDiagnostic(buf);
         std::string msg{buf.begin(), buf.end()};
 
-        FullSourceLoc loc(info.getLocation(), session.getCompiler().getSourceManager());
-
+        FullSourceLoc loc(info.getLocation(), sm);
         session.pushWarning(std::to_string(loc.getLineNumber()) + ":" + msg.data());
+
         return true;
     }
 };
