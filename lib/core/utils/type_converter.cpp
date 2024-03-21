e3b0dda3fb1125cf574f3e284bf567d2f97d5768
Pavlo Hilei <phile@softserveinc.com>
2024-03-21 18:35:37 +0100

    Fix more includes

--- lib/core/utils/type_converter.cpp
+++ lib/core/utils/type_converter.cpp
@@ -1,10 +1,8 @@
-#include "core/utils/type_converter.h"
+#include <oklt/core/kernel_metadata.h>
 #include <oklt/core/error.h>
+#include "core/utils/type_converter.h"
 
 #include <clang/AST/AST.h>
-#include <memory>
-#include "clang/AST/Type.h"
-#include "oklt/core/kernel_metadata.h"
 
 namespace oklt {
 using namespace clang;
