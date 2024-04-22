ebe9992e6891ed89ffae9d9fc422b04401757ab5
Pavlo Hilei <phile@softserveinc.com>
2024-04-22 14:06:39 +0200

    Add restrict return type test for serial and openmp

---
+++ tests/functional/data/transpiler/backends/openmp/restrict/restrict_return_type_ref.cpp
@@ -0,0 +1,11 @@
+float *__restrict__ myfn(float *a) { return a + 1; }
+
+float *myfn2(float *a) { return a + 1; }
+
+extern "C" void hello() {
+#pragma omp parallel for
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++) {
+    }
+  }
+}
