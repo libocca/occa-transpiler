521ee9d96be92627c410f550e054f78108efd896
Pavlo Hilei <phile@softserveinc.com>
2024-04-22 13:43:13 +0200

    Fix @restrict handle for dpcpp backend. Add more restrict tests

--- lib/attributes/backend/dpcpp/restrict.cpp
+++ lib/attributes/backend/dpcpp/restrict.cpp
@@ -10,37 +10,14 @@
 namespace {
 using namespace oklt;
 using namespace clang;
-const std::string RESTRICT_MODIFIER = "__restrict__";
+const std::string RESTRICT_MODIFIER = "__restrict__ ";
 
 HandleResult handleRestrictAttribute(const clang::Attr& a,
                                      const clang::Decl& decl,
                                      SessionStage& s) {
     SPDLOG_DEBUG("Handle [@restrict] attribute");
-
-    SourceLocation identifierLoc = decl.getLocation();
-    std::string restrictText = " " + RESTRICT_MODIFIER + " ";
-
-    // INFO: might be better to use rewriter.getRewrittenText() method
-
-    auto& ctx = decl.getASTContext();
-    SourceRange r1(decl.getSourceRange().getBegin(), identifierLoc);
-    auto part1 = getSourceText(r1, ctx);
-
-    auto ident = [](const clang::Decl& decl) {
-        switch (decl.getKind()) {
-            case Decl::Field:
-                return cast<FieldDecl>(decl).getNameAsString();
-            case Decl::ParmVar:
-                return cast<ParmVarDecl>(decl).getQualifiedNameAsString();
-            default:
-                return cast<VarDecl>(decl).getQualifiedNameAsString();
-        }
-    }(decl);
-
-    std::string modifiedArgument = part1 + restrictText + ident;
-
-    s.getRewriter().ReplaceText({getAttrFullSourceRange(a).getBegin(), decl.getEndLoc()},
-                                part1 + restrictText + ident);
+    removeAttribute(a, s);
+    s.getRewriter().InsertTextBefore(decl.getLocation(), RESTRICT_MODIFIER);
     return {};
 }
 
