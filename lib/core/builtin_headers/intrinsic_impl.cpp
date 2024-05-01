33657c8d0ebeeeb3592eb5ac7653f980de524be6
Viktor Yastrebov <v.yastrebov90@gmail.com>
2024-05-01 12:26:09 +0300

    fix for code review, addInstrinsicStub use the same VFS

--- lib/core/builtin_headers/intrinsic_impl.cpp
+++ lib/core/builtin_headers/intrinsic_impl.cpp
@@ -4,11 +4,13 @@
 #include "core/builtin_headers/okl_intrinsic_hip.h"
 #include "core/builtin_headers/okl_intrinsic_host.h"
 
+#include <clang/Frontend/CompilerInstance.h>
 #include "core/transpiler_session/transpiler_session.h"
 #include "core/vfs/overlay_fs.h"
-#include <clang/Frontend/CompilerInstance.h>
 
 namespace oklt {
+
+using namespace llvm;
 
 struct IntrinsicInfo {
     std::string source;
@@ -18,55 +20,52 @@ struct IntrinsicInfo {
 IntrinsicInfo getIntrinsicInfo(TargetBackend backend) {
     switch (backend) {
         case TargetBackend::CUDA:
-            return IntrinsicInfo {
-                std::string(INTRINSIC_CUDA),
-                {"cuda_pipeline_primitives.h"}
-            };
+            return IntrinsicInfo{std::string(INTRINSIC_CUDA), {"cuda_pipeline_primitives.h"}};
         case TargetBackend::DPCPP:
-            return IntrinsicInfo {
-                std::string(INTRINSIC_DPCPP),
-                {}
-            };
+            return IntrinsicInfo{std::string(INTRINSIC_DPCPP), {}};
         case TargetBackend::HIP:
-            return IntrinsicInfo {
-                std::string(INTRINSIC_HIP),
-                {}
-            };
+            return IntrinsicInfo{std::string(INTRINSIC_HIP), {}};
         case TargetBackend::OPENMP:
         case TargetBackend::SERIAL:
         case TargetBackend::_LAUNCHER:
-            return IntrinsicInfo {
-                std::string(INTRINSIC_HOST),
-                {
-                 "stddef.h",
-                 "cmath",
-                 "cstring",
-                }
-            };
+            return IntrinsicInfo{std::string(INTRINSIC_HOST),
+                                 {
+                                     "stddef.h",
+                                     "cmath",
+                                     "cstring",
+                                 }};
         default:
             return {{}, {}};
     }
 }
 
-void addInstrinsicStub(TranspilerSession &session,
-                       clang::CompilerInstance &compiler)
-{
+void addInstrinsicStub(TranspilerSession& session, clang::CompilerInstance& compiler) {
     auto& headers = session.getStagedHeaders();
-    headers.emplace(INTRINSIC_INCLUDE_FILENAME,std::string());
+    headers.emplace(INTRINSIC_INCLUDE_FILENAME, std::string());
     auto& fm = compiler.getFileManager();
     auto vfs = fm.getVirtualFileSystemPtr();
+    if (vfs) {
+        vfs::OverlayFileSystem* ptr = dynamic_cast<vfs::OverlayFileSystem*>(vfs.get());
 
-    auto overlayFs = makeOverlayFs(vfs, headers);
-    fm.setVirtualFileSystem(overlayFs);
+        for (const auto& elem : headers) {
+            for (auto& overlay : ptr->overlays_range()) {
+                vfs::InMemoryFileSystem* inMemory =
+                    dynamic_cast<vfs::InMemoryFileSystem*>(overlay.get());
+                if (inMemory && !inMemory->exists(elem.first)) {
+                    inMemory->addFile(elem.first, 0, MemoryBuffer::getMemBuffer(elem.second));
+                }
+            }
+        }
+    } else {
+        auto overlayFs = makeOverlayFs(vfs, headers);
+        fm.setVirtualFileSystem(overlayFs);
+    }
 }
 
-std::vector<std::string> embedInstrinsic(std::string &input,
-                                         TargetBackend backend)
-{
+std::vector<std::string> embedInstrinsic(std::string& input, TargetBackend backend) {
     auto info = getIntrinsicInfo(backend);
     input.insert(0, info.source);
     return info.includes;
 }
-
 
 }  // namespace oklt
