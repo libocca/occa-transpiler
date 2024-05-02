6335f1637a321f4321a06daf82a98537f6b15b97
Viktor Yastrebov <vast@softserveinc.com>
2024-05-02 13:24:47 +0300

    correct the references for the transpile common tests, includes suite

--- tests/functional/data/transpiler/common/includes/bug_165_ref.cpp
+++ tests/functional/data/transpiler/common/includes/bug_165_ref.cpp
@@ -1,10 +1,10 @@
-#include <cuda_runtime.h>
-#include <utility>
 #include <vector>
+#include <utility>
+#include <cuda_runtime.h>
 
 extern "C" __global__ __launch_bounds__(10) void _occa_hello_0() {
-    {
-        int i = (0) + blockIdx.x;
-        { int j = (0) + threadIdx.x; }
-    }
+  {
+    int i = (0) + blockIdx.x;
+    { int j = (0) + threadIdx.x; }
+  }
 }
--- tests/functional/data/transpiler/common/includes/kernel_ref.cpp
+++ tests/functional/data/transpiler/common/includes/kernel_ref.cpp
@@ -1,16 +1,20 @@
 #include <cuda_runtime.h>
 
 struct Data {
-    float* __restrict__ data;
-    int* __restrict__ idxs;
+  float *__restrict__ data;
+  int *__restrict__ idxs;
 };
 
 __constant__ int SIZE = 128;
 __device__ float add(float a, float b);
 
-__device__ float add2(float a, float b) {
-    return a + b;
+__device__ float add2(float a, float b) { return a + b; }
+
+extern "C" __global__
+__launch_bounds__(1) void _occa_function1_0(const Data data1,
+                                            const Data data2) {
+  {
+    int i = (0) + blockIdx.x;
+    { int j = (0) + threadIdx.x; }
+  }
 }
-
-
-extern "C" __global__ void _occa_function1_0(const Data data1, const Data data2) {}
