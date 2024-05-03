1d42d28ec9f23e7971e5f846b66aa61e837be270
Pavlo Hilei <phile@softserveinc.com>
2024-05-03 14:52:29 +0200

    Update nobarrier tests for priority issue case

--- tests/functional/data/transpiler/backends/hip/nobarrier/nobarrier_builtin_ref.cpp
+++ tests/functional/data/transpiler/backends/hip/nobarrier/nobarrier_builtin_ref.cpp
@@ -1,26 +1,41 @@
 #include <hip/hip_runtime.h>
 
 extern "C" __global__ __launch_bounds__(10) void _occa_hello_kern_0() {
+  {
+    int i = (0) + blockIdx.x;
+    __shared__ int shm[10];
     {
-        int i = (0) + blockIdx.x;
-        __shared__ int shm[10];
-        {
-            int j = (0) + threadIdx.x;
-            shm[j] = j;
-        }
-        __syncthreads();
-        {
-            int j = (0) + threadIdx.x;
-            shm[j] = j;
-        }
-        {
-            int j = (0) + threadIdx.x;
-            shm[j] = j;
-        }
-        __syncthreads();
-        {
-            int j = (0) + threadIdx.x;
-            shm[j] = j;
-        }
+      int j = (0) + threadIdx.x;
+      shm[j] = j;
     }
+    __syncthreads();
+    {
+      int j = (0) + threadIdx.x;
+      shm[j] = j;
+    }
+    {
+      int j = (0) + threadIdx.x;
+      shm[j] = j;
+    }
+    __syncthreads();
+    {
+      int j = (0) + threadIdx.x;
+      shm[j] = j;
+    }
+  }
+}
+
+extern "C" __global__ __launch_bounds__(32) void _occa_priority_issue_0() {
+  {
+    int i = (0) + blockIdx.x;
+    __shared__ float shm[32];
+    {
+      int j = (0) + threadIdx.x;
+      shm[i] = i;
+    }
+    {
+      int j = (0) + threadIdx.x;
+      atomicAdd(&(shm[i * j]), 32);
+    }
+  }
 }
--- tests/functional/data/transpiler/backends/serial/nobarrier/nobarrier_builtin_ref.cpp
+++ tests/functional/data/transpiler/backends/serial/nobarrier/nobarrier_builtin_ref.cpp
@@ -1,17 +1,29 @@
 extern "C" void hello_kern() {
-    for (int i = 0; i < 10; ++i) {
-        int shm[10];
-        for (int j = 0; j < 10; ++j) {
-            shm[j] = j;
-        }
-        for (int j = 0; j < 10; ++j) {
-            shm[j] = j;
-        }
-        for (int j = 0; j < 10; ++j) {
-            shm[j] = j;
-        }
-        for (int j = 0; j < 10; ++j) {
-            shm[j] = j;
-        }
+  for (int i = 0; i < 10; ++i) {
+    int shm[10];
+    for (int j = 0; j < 10; ++j) {
+      shm[j] = j;
     }
+    for (int j = 0; j < 10; ++j) {
+      shm[j] = j;
+    }
+    for (int j = 0; j < 10; ++j) {
+      shm[j] = j;
+    }
+    for (int j = 0; j < 10; ++j) {
+      shm[j] = j;
+    }
+  }
+}
+
+extern "C" void priority_issue() {
+  for (int i = 0; i < 32; ++i) {
+    float shm[32];
+    for (int j = 0; j < 32; ++j) {
+      shm[i] = i;
+    }
+    for (int j = 0; j < 32; ++j) {
+      shm[i * j] += 32;
+    }
+  }
 }
