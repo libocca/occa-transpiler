3050a2b00a727105060529325bf801558472a51a
kchabSS <156786434+kchabSS@users.noreply.github.com>
2024-05-10 20:14:47 +0200

    Update shared_in_typedecl_ref.cpp

--- tests/functional/data/transpiler/backends/cuda/shared/shared_in_typedecl_ref.cpp
+++ tests/functional/data/transpiler/backends/cuda/shared/shared_in_typedecl_ref.cpp
@@ -4,8 +4,8 @@ typedef __shared__ float sh_float32_t;
 extern "C" __global__ __launch_bounds__(32) void _occa_test_kernel_0() {
   {
     int i = (0) + blockIdx.x;
-      __shared__ sh_float32_t b[32];
-      {
+    __shared__ sh_float32_t b[32];
+    {
       int j = (0) + threadIdx.x;
       b[j] = i + j;
     }
