29c8c79e4db4cd59c749879be8d084cc36974ea0
Iurii Kobein <ikobein@softserveinc.com>
2024-08-01 20:00:11 +0100

    add missed handlers for restrict attr

--- lib/attributes/backend/serial/restrict.cpp
+++ lib/attributes/backend/serial/restrict.cpp
@@ -1,4 +1,5 @@
 #include "attributes/backend/serial/common.h"
+#include "attributes/utils/default_handlers.h"
 
 #include <spdlog/spdlog.h>
 
@@ -9,6 +10,8 @@ using namespace clang;
 __attribute__((constructor)) void registerOPENMPRestrictHandler() {
     auto ok = registerBackendHandler(
         TargetBackend::SERIAL, RESTRICT_ATTR_NAME, serial_subset::handleRestrictAttribute);
+
+    ok &= registerBackendHandler(TargetBackend::SERIAL, RESTRICT_ATTR_NAME, emptyHandleStmtAttribute);
 
     if (!ok) {
         SPDLOG_ERROR("[SERIAL] Failed to register {} attribute handler", RESTRICT_ATTR_NAME);
